f5bdd930586285d62ef074901c04ea8535837040
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2021-09-24 09:13:43 +0300

    Create programmeerimise märkmed.js

---
+++ programmeerimise märkmed.js
@@ -0,0 +1,37 @@
+// CRUD
+
+// create
+[]
+["test",2,3,null]
+console.log([])
+let list= []
+let list=["test",2,3,null]
+
+
+// read
+//#region
+list[0] // "test"
+list[1] // 2
+list[2] // 3
+list[3] // null
+list[4] // undefined
+list.length // mitu elementi listis(arrays) on
+
+let fruits= ["orange", "pineapple", "apple"]
+fruits.indexOf("pear") //-1 // näitab mitmes see on arrays
+fruits.indexOf("apple") // 2
+fruits.includes("pear) //false // näitab kas arrays on see asi või mitte
+list.filter(elem=>elem>0) // 
+//#endregion
+
+
+//update
+list.push("kiwi")
+fruits.push("kiwi")
+fruits[4]="watermelon" // vahetab välja
+fruits.splice(2,0,"plum") // 2 näitab kuhu lisada, 0 näitab mitu numbrit kustutada
+
+//delete
+fruits.pop() // võtab viimase elemendi välja
+fruits.splice(2,1) // võtab teise ja kustutab selle (1)
+
